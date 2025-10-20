814677ade50cdb697b0941b8d41f49734196be3a
Filipe C <filipecavalcantiximenes@gmail.com>
2025-10-20 13:50:22 -0300

    :sparkles: added: user entity

---
+++ src/Core/Domain/Entities/User.ts
@@ -0,0 +1,50 @@
+import { InvalidValueError } from "../Errors/InvalidValueError";
+import { Id } from "../ValueObjects/Id";
+
+export interface UserProps {
+    id: Id,
+    username: string,
+    password: string
+}
+
+export class User {
+    constructor(private userProps: UserProps) {
+        this.validateUsername(userProps.username);
+        this.validatePassword(userProps.password);
+    }
+
+    protected get id(): Id {
+        return this.userProps.id;
+    }
+
+    protected get username(): string {
+        return this.userProps.username;
+    }
+
+    protected get password(): string {
+        return this.userProps.password
+    }
+
+    protected changeUsername(newUsername: string) {
+        this.userProps.username = newUsername;
+    }
+
+    protected changePassword(newPassword: string) {
+        this.userProps.password = newPassword;
+    }
+
+    protected validateUsername(username: string) {
+        if (username.length < 3) {
+            throw new InvalidValueError("O username deve ter pelo menos três caracteres");
+        }
+        if (username.length > 150) {
+            throw new InvalidValueError("Pra quê um nome tão grande desse?");
+        }
+    }
+
+    protected validatePassword(password: string) {
+        if (password.length < 4) {
+            throw new InvalidValueError("A senha deve ter pelo menos 4 caracteres");
+        }
+    }
+}
