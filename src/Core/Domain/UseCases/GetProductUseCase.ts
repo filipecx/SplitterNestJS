08b5f01df6ea1857f231f0a6c28f6510c91bd9c8
Filipe C <filipecavalcantiximenes@gmail.com>
2025-10-21 16:19:43 -0300

    :sparkles: added: nestjs inject and injectable

--- src/Core/Domain/UseCases/GetProductUseCase.ts
+++ src/Core/Domain/UseCases/GetProductUseCase.ts
@@ -1,9 +1,11 @@
-import { IntProductRepository } from "../../../Infrastructure/Interfaces/IntProductRepository";
+import { Inject, Injectable } from "@nestjs/common";
+import type { IntProductRepository } from "../../../Infrastructure/Interfaces/IntProductRepository";
 import { ProductItem } from "../Entities/ProductItem";
 import { Id } from "../ValueObjects/Id";
 
+@Injectable()
 export class GetProductUseCase {
-    constructor(private repository: IntProductRepository){}
+    constructor(@Inject('IntProductRepository')private repository: IntProductRepository){}
     async execute(id: Id): Promise<ProductItem>{
         const product = await this.repository.get(id);
         return product;
