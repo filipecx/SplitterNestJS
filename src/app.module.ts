73117f08320644806d679c659ef53879d7d6a708
Filipe C <filipecavalcantiximenes@gmail.com>
2025-10-22 17:39:49 -0300

    :recycle: added: controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,26 +9,62 @@ import { ProductRepository } from './Infrastructure/Repositories/ProductReposito
 import { PaymentMethodFactory } from './Infrastructure/Factories/PaymentMethodFactory';
 import { RabbitProducer } from './Infrastructure/Communication/RabbitMQPublisher';
 import { SendSaleController } from './Web/Controllers/send-sale-controller';
-import { CreateProductController } from './Web/Controllers/create-product-controller';
+import { CreateProductController } from './Web/Controllers/Product/create-product-controller';
 import { AddProductItemUseCase } from './Core/Domain/UseCases/AddProductItemUseCase';
 import { GetAllProductsUseCase } from './Core/Domain/UseCases/GetAllProductsUseCase';
 import { GetArtisanUseCase } from './Core/Domain/UseCases/GetArtisanUseCase';
 import { ChunkMapper } from './Infrastructure/Mappers/SaleChunkMapper';
 import { QRCodeGenerator } from './Infrastructure/QRCode/qrCodeGenerator';
-import { GetProductController } from './Web/Controllers/get-product-controller';
+import { GetProductController } from './Web/Controllers/Product/get-product-controller';
 import { GetProductUseCase } from './Core/Domain/UseCases/GetProductUseCase';
-import { GetAllProductsController } from './Web/Controllers/get-allProducts-controller';
+import { GetAllProductsController } from './Web/Controllers/Product/get-allProducts-controller';
+import { UpdateArtisanController } from './Web/Controllers/Artisan/update-artisan-controller';
+import { UpdateArtisanStatusController } from './Web/Controllers/Artisan/change-artisan-status-controller';
+import { DeleteArtisanController } from './Web/Controllers/Artisan/delete-artisan-controller';
+import { GetAllArtisanController } from './Web/Controllers/Artisan/get-all-artisan-controller';
+import { GetAllActiveArtisanController } from './Web/Controllers/Artisan/get-all-active-artisans-constroller';
+import { GetAllArtisanUseCase } from './Core/Domain/UseCases/GetAllArtisanUseCase';
+import { GetAllAcitveArtisansUseCase } from './Core/Domain/UseCases/GetAllActiveArtisansUseCase';
+import { DeleteArtisanUseCase } from './Core/Domain/UseCases/DeleteArtisanUseCase';
+import { UpdateArtisanUseCase } from './Core/Domain/UseCases/UpdateArtisanUseCase';
+import { UpdateProductUseCase } from './Core/Domain/UseCases/UpdateProductUseCase';
+import { UpdateProductController } from './Web/Controllers/Product/update-product-controller';
+import { DeleteProductController } from './Web/Controllers/Product/delete-product-controller';
+import { DeleteProductUseCase } from './Core/Domain/UseCases/DeleteProductUseCase';
+import { DeductStockQuantityController } from './Web/Controllers/Product/deduct-stock-quantity-controller';
+import { DeductStockUseCase } from './Core/Domain/UseCases/DeductStockUseCase';
 
 @Module({
   imports: [],
-  controllers: [CreateArtisanController, SendSaleController, CreateProductController, GetProductController, GetAllProductsController],
+  controllers: [
+    CreateArtisanController, 
+    UpdateArtisanController, 
+    UpdateArtisanStatusController,
+    DeleteArtisanController,
+    GetAllArtisanController,
+    GetAllActiveArtisanController, 
+    SendSaleController, 
+    CreateProductController, 
+    GetProductController, 
+    GetAllProductsController,
+    UpdateProductController,
+    DeleteProductController,
+    DeductStockQuantityController,
+  ],
   providers: [
     PrismaService, 
     AddArtisanUseCase, 
+    GetArtisanUseCase,
+    GetAllArtisanUseCase,
+    GetAllAcitveArtisansUseCase,
+    DeleteArtisanUseCase,
+    UpdateArtisanUseCase,
     RegisterSaleUseCase,
     AddProductItemUseCase,
     GetAllProductsUseCase,
-    GetArtisanUseCase,
+    UpdateProductUseCase,
+    DeleteProductUseCase,
+    DeductStockUseCase,   
     ChunkMapper,
     QRCodeGenerator,
     GetProductUseCase,
